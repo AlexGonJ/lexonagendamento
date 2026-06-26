82045c3dbe282786f1e9dc59b3763f9fa783ced2
AlexGonJ <alexgoncalvesdtna@gmail.com>
2026-06-26 19:18:13 -0300

    fix erros de build

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,6 +3,9 @@ import prisma from '@/lib/prisma';
 import { redirect } from 'next/navigation';
 import AdminLayoutClient from './AdminLayoutClient';
 
+export const dynamic = 'force-dynamic';
+
+
 export default async function AdminLayout({
   children,
 }: {
